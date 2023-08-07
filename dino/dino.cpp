05d88f55e099b8bb1d4dd904f4274076e4ae87bc
natalia <121969070+natrand@users.noreply.github.com>
2023-08-07 20:36:17 +0200

    updated file

--- dino/dino.cpp
+++ dino/dino.cpp
@@ -5,47 +5,31 @@
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
+#include "Game.h"
 
-//setting fundamentals
 
-float x = 0;
-float y = 0;
-float velocityX = 0;
-float velocityY = 0;
-float accX = 0;
-float accY = 0;
-float gravity = 1;
-
-void update() {
-    if (y < 500)
-    {
-        velocityY += gravity;
-    }
-    else if (y > 500)
-    {
-        y = 500;
-    }
-    velocityX += accX;
-    velocityY += accY;
-
-    x += velocityX;
-    y += velocityY;
-}
 
 int main()
 {
+
+   
     //window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Dino Game");
     sf::Event ev;
+    sf::View view(sf::FloatRect(0.f, 0.f, 800.f, 600.f)); //control view within the window
+
     window.setFramerateLimit(60);
+
     //setting up square
+
     sf::RectangleShape rectangle;
     rectangle.setSize(sf::Vector2f(50, 50));
     rectangle.setFillColor(sf::Color::Red);
     rectangle.setPosition(0, window.getSize().y - rectangle.getSize().y);
-    // rectangle.setPosition(-230, 100);
 
+    Game game;
 
+    bool isJumping = false;
      //game loop
     while (window.isOpen())
     {
@@ -61,31 +45,41 @@ int main()
                 if (ev.key.code == sf::Keyboard::Escape) //to exit we have to press esc button
                     window.close();
                 //jumping
-                else if (ev.type == sf::Event::KeyPressed && ev.key.code == sf::Keyboard::Space)
-                {
-                    velocityY = -10;
+                else if (ev.key.code == sf::Keyboard::Space && !isJumping)
+                {   
+                    game.update(window);
+                    game.velocityY = -10;
+                    isJumping = true;
+                }
+                break;
+            case sf::Event::KeyReleased:
+                if (ev.key.code == sf::Keyboard::Space) {
+                    isJumping = false;
                 }
                 break;
             }
 
         }
-        //moving object left and right
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        
+        float moveAmount = 5.f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         {
-            velocityX = -2;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            velocityX = 2;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
+            game.velocityX = 2;
+            rectangle.move(moveAmount, 0.f);
 
+            if (rectangle.getPosition().x + rectangle.getSize().x >= view.getCenter().x + view.getSize().x / 2) {
+                view.move(moveAmount, 0.f); // Move the view to the right
+            }
+            
         }
-        update();
-        rectangle.setPosition(x, y);
+        else {
+           game.velocityX = 0;
+        }
+       
+        game.update(window);
+        rectangle.setPosition(game.x, game.y);
 
-
+        window.setView(view);
         window.clear(sf::Color::Black);
         window.draw(rectangle);
         window.display();
